d6fd15b5a91f6337b7e1977243f0694a4daddb69
Tatiana Kazakova <test.kazakova@gmail.com>
2022-12-03 10:15:43 +0300

    1

--- src/main/java/ui/flow/SomeTableFlow.java
+++ src/main/java/ui/flow/SomeTableFlow.java
@@ -8,33 +8,35 @@ import org.assertj.core.api.Assertions;
 import ui.pom.elements.Column;
 import ui.pom.pages.SomeTable;
 
+import static com.codeborne.selenide.Selenide.page;
+
 public class SomeTableFlow {
     public SomeTableFlow checkColumnName(Integer integer, String expectColumnName){
-        Assertions.assertThat(new Column(Selenide.page(SomeTable.class)
+        Assertions.assertThat(new Column(page(SomeTable.class)
                         .getTrelloColumn().get(integer)).getColumnTitle()).contains(expectColumnName);
         return this;
     }
     public SomeTableFlow clickAddCard(Integer integer){
-        new Column(Selenide.page(SomeTable.class).getTrelloColumn().get(integer)).clickAddCardButton();
+        new Column(page(SomeTable.class).getTrelloColumn().get(integer)).clickAddCardButton();
         return this;
     }
     public SomeTableFlow setTextInNewCard(Integer integer, String text){
-        new Column(Selenide.page(SomeTable.class).getTrelloColumn().get(integer)).selValInNexCard(text);
+        new Column(page(SomeTable.class).getTrelloColumn().get(integer)).selValInNexCard(text);
         return this;
     }
 
     public SomeTableFlow checkColumnContent(Integer integer, String containText){
-        new Column(Selenide.page(SomeTable.class).getTrelloColumn().get(integer)).getRoot()
+        new Column(page(SomeTable.class).getTrelloColumn().get(integer)).getRoot()
                 .shouldHave(Condition.text(containText));
         return this;
     }
     public SomeTableFlow confirmAddedText(Integer integer){
-        new Column(Selenide.page(SomeTable.class).getTrelloColumn().get(integer)).clickConfirmAddedNewCard();
+        new Column(page(SomeTable.class).getTrelloColumn().get(integer)).clickConfirmAddedNewCard();
         return this;
     }
 
     private SelenideElement getColumnString(String columnName){
-        return Selenide.page(SomeTable.class).getTrelloColumn().findBy(Condition.text(columnName));
+        return page(SomeTable.class).getTrelloColumn().findBy(Condition.text(columnName));
     }
 
     public SomeTableFlow test(String str){
@@ -43,9 +45,19 @@ public class SomeTableFlow {
     }
 
     public SomeTableFlow clickAddCard (String columnName){
-        new Column(getColumnString(columnName)).clickAddCardButton();
+        final Column columnElement = page(SomeTable.class).getTrelloColumn().stream()
+                .map(Column::new)
+                .filter(column -> column.getColumnTitle().equals(columnName))
+                .findFirst()
+                .orElseThrow();
+        columnElement.clickAddCardButton();
         return this;
     }
+//
+//    public SomeTableFlow clickAddCard (String columnName){
+//        new Column(getColumnString(columnName)).clickAddCardButton();
+//        return this;
+//    }
 
     public SomeTableFlow setTextInNewCard(String columnName, String text){
         new Column(getColumnString(columnName)).selValInNexCard(text);
@@ -58,6 +70,16 @@ public class SomeTableFlow {
 
     public SomeTableFlow checkColumnContent(String columnName, String containText){
         new Column(getColumnString(columnName)).getRoot().shouldHave(Condition.text(containText));
+        return this;
+    }
+
+    public SomeTableFlow clickAddCardButtonInColumnWithTitle(String columnTitle) {
+        final Column columnElement = page(SomeTable.class).getTrelloColumn().stream()
+                .map(Column::new)
+                .filter(column -> column.getColumnTitle().equals(columnTitle))
+                .findFirst()
+                .orElseThrow();
+        columnElement.clickAddCardButton();
         return this;
     }
 }
