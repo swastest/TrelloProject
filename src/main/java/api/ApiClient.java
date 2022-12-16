c2d5f35f85e1d2cd5e0ed40b86935e985196a521
Tatyana Kazakova <tatikzk@MBP-Tatyana.Dlink>
2022-12-17 00:52:07 +0300

    added ApiClient

---
+++ src/main/java/api/ApiClient.java
@@ -0,0 +1,73 @@
+package api;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import utils.testData.EndPoints;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.config.EncoderConfig.encoderConfig;
+import static io.restassured.config.RestAssuredConfig.newConfig;
+
+public class ApiClient {
+    private RequestSpecBuilder requestSpecBuilder = new RequestSpecBuilder()
+            .setConfig(newConfig().encoderConfig(encoderConfig()
+                    .defaultContentCharset(StandardCharsets.UTF_8)))
+            .setBaseUri("https://api.trello.com")
+            .log(LogDetail.ALL);
+
+    private RequestSpecification requestSpec;
+
+    public Response sendRequest(Method method, int expectedStatusCode, EndPoints endPoints, Object... params) {
+        return given()
+                .spec(requestSpec)
+                .request(method, endPoints.getPath(), params)
+                .then().log().all().statusCode(expectedStatusCode).extract().response();
+    }
+
+    public ApiClient build() {
+        requestSpec = requestSpecBuilder.build();
+        return this;
+    }
+
+    public ApiClient addQueryParam(String key, String value) {
+        requestSpecBuilder.addQueryParam(key, value);
+        return this;
+    }
+
+    public ApiClient addBody(String body) {
+        requestSpecBuilder.setBody(body);
+        return this;
+    }
+
+    public ApiClient addBody(Map<String, String> body) {
+        requestSpecBuilder.setBody(body);
+        return this;
+    }
+
+    public ApiClient addBody(Object body) {
+        requestSpecBuilder.setBody(body);
+        return this;
+    }
+
+    public ApiClient addContentTypeJson(){
+        requestSpecBuilder.setContentType(ContentType.JSON);
+        return this;
+    }
+
+    public ApiClient addContentTypeForFormParam(){
+        requestSpecBuilder.setContentType(ContentType.URLENC);
+        return this;
+    }
+
+    public ApiClient addFormParam(String key, String value){
+        requestSpecBuilder.addFormParam(key, value);
+        return this;
+    }
+}
--- src/test/java/ui/Tests.java
+++ src/test/java/ui/Tests.java
@@ -1,15 +1,19 @@
 package ui;
 
+import api.ApiClient;
 import com.codeborne.selenide.Condition;
+import io.restassured.http.Method;
 import org.junit.jupiter.api.Test;
 import utils.PagesUtils;
+import utils.testData.EndPoints;
+import utils.testData.Users;
 
 import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class Tests extends PagesUtils {
     @Test
-    void test01(){
+    void test01() {
         open("https://trello.com");
         mainPageFlow.clickSingInButton();
         singInPageFlow.setValueEmailLogin(userConfig.email())
@@ -17,8 +21,18 @@ public class Tests extends PagesUtils {
                 .setValuePassword(userConfig.pass())
                 .clickSubmitLoginButton();
         mainBoardPageFlow.clickOnTable();
-       $$("div#board .list").findBy(Condition.text("В процессе")).$("a.open-card-composer").click();
-       int a = 0;
+        $$("div#board .list").findBy(Condition.text("В процессе")).$("a.open-card-composer").click();
+        int a = 0;
+    }
+
+    @Test
+    void test02() {
+        ApiClient api = new ApiClient();
+        api.addContentTypeJson()
+                .addQueryParam("key", Users.TEST_USER_1.getKey())
+                .addQueryParam("token", Users.TEST_USER_1.getToken())
+                .build()
+                .sendRequest(Method.GET,200, EndPoints.ALL_BOARDS);
     }
 
 }
